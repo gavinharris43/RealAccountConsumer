5689b241b5f09eb1eff9eecc22404963c4e8d8fe
gavinharris43 <gavinharris43@googlemail.com>
2019-02-15 15:45:03 +0000

    add

--- src/main/java/com/qa/account/accountapi/persistence/domain/SentAccount.java
+++ src/main/java/com/qa/account/accountapi/persistence/domain/SentAccount.java
@@ -7,23 +7,64 @@ public class SentAccount {
     private String firstName;
 
     private String lastName;
-
+    private String email;
+    
+    private String password;
     private String accountNumber;
+    private SentCohort sentCohort;
 
     private SentPrize sentPrize;
+   
+
+    
+
+      public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
     public SentAccount() {
     }
 
-    public SentAccount(Long accountId, String firstName, String lastName, String accountNumber, SentPrize sentPrize) {
+    public SentAccount(Long accountId, String firstName, String lastName, String accountNumber,String email,String password,SentCohort sentCohort, SentPrize sentPrize) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.accountNumber = accountNumber;
         this.accountId = accountId;
+        this.email=email;
+        this.password=password;
+        this.sentCohort = sentCohort;
         this.sentPrize = sentPrize;
     }
 
-    public Long getId() {
+    public Long getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(Long accountId) {
+		this.accountId = accountId;
+	}
+
+	public SentCohort getSentCohotr() {
+		return sentCohort;
+	}
+
+	public void setSentCohotr(SentCohort sentCohotr) {
+		this.sentCohort = sentCohotr;
+	}
+
+	public Long getId() {
         return accountId;
     }
 
---
+++ src/main/java/com/qa/account/accountapi/persistence/domain/SentCohort.java
@@ -0,0 +1,39 @@
+package com.qa.account.accountapi.persistence.domain;
+
+public class SentCohort {
+
+
+    private Long cohortId;
+
+    private int trainerId;
+
+    private String name;
+
+    public Long getCohortId() {
+		return cohortId;
+	}
+
+	public void setCohortId(Long cohortId) {
+		this.cohortId = cohortId;
+	}
+
+	public int getTrainerId() {
+		return trainerId;
+	}
+
+	public void setTrainerId(int trainerId) {
+		this.trainerId = trainerId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+
+}
